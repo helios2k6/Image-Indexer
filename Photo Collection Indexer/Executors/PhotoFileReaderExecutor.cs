24d77d3c765003101f2f0b44fc5b00b5502088c3
Andrew Johnson <helios2k6@gmail.com>
2017-06-20 23:08:07 -0700

    Remove Unneeded Using

--- Photo Collection Indexer/Executors/PhotoFileReaderExecutor.cs
+++ Photo Collection Indexer/Executors/PhotoFileReaderExecutor.cs
@@ -19,7 +19,6 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-using Core.Media;
 using PhotoCollectionIndexer.Utils;
 using System;
 using System.Collections.Concurrent;
